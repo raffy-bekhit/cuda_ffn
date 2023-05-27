3ce0d70242e2e9fdf5dcc0316d7d918a7305f9d4
raffybekhit <raffybekhit@gmail.com>
2023-05-27 18:01:11 +0000

    add working changes

---
+++ run.sh
@@ -0,0 +1,4 @@
+cmake --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCUDA_ROOT=/usr/local/cuda -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=gcc -DCMAKE_CXX_COMPILER:FILEPATH=g++ -S. -B./build -G "Unix Makefiles" ;
+cmake --build ./build --config Debug --target clean -j 18 -- ;
+cmake --build ./build --config Debug --target all -j 18 -- ;
+./build/src/ffn.exe;
--- src/ffn.cu
+++ src/ffn.cu
@@ -2,16 +2,16 @@
 #include <cuda_runtime.h>
 #include <cudnn.h>
 #include <tuple>
-#include <cublas.h>
+#include <cublas_v2.h>
 
 using namespace std;
 
-#define HX 4
-#define WX 4
-#define HW 4
-#define WW 4
-#define HY 4
-#define WY 4
+#define HX 5 //4 //
+#define WX 4 //4 //
+#define HW 4 //4 //
+#define WW 2 //4 //
+#define HY HX //4 //
+#define WY WW //4 //
 
 __host__ void OutputHWSpecs()
 {
@@ -24,6 +24,44 @@ __host__ void OutputHWSpecs()
     cudaGetDevice(&device);
     cudaGetDeviceProperties(&devProp, device);
     std::cout << "Compute capability:" << devProp.major << "." << devProp.minor << std::endl;
+}
+
+
+__host__ cudnnTensorDescriptor_t createTensorDescriptor(int h, int w)
+{
+    // create the tensor descriptor
+    cudnnDataType_t dtype = CUDNN_DATA_FLOAT;
+    cudnnTensorFormat_t format = CUDNN_TENSOR_NCHW;
+    int n = 1, c = 1; //, h = 1, w = 10;
+    cudnnTensorDescriptor_t x_desc;
+    cudnnCreateTensorDescriptor(&x_desc);
+    cudnnSetTensor4dDescriptor(x_desc, format, dtype, n, c, h, w);
+
+    return x_desc;
+}
+
+__host__ float *runCuDnnActivationForward(cudnnHandle_t handle_, cudnnTensorDescriptor_t x_desc, float *x)
+{
+    // create activation function descriptor
+    float alpha[1] = {1.0};
+    float beta[1] = {0.0};
+    cudnnActivationDescriptor_t sigmoid_activation;
+    cudnnActivationMode_t mode = CUDNN_ACTIVATION_SIGMOID;
+    cudnnNanPropagation_t prop = CUDNN_NOT_PROPAGATE_NAN;
+    cudnnCreateActivationDescriptor(&sigmoid_activation);
+    cudnnSetActivationDescriptor(sigmoid_activation, mode, prop, 0.0f);
+
+    cudnnActivationForward(
+        handle_,
+        sigmoid_activation,
+        alpha,
+        x_desc,
+        x,
+        beta,
+        x_desc,
+        x);
+
+    return x;
 }
 
 __host__ float* initHostMatrix(int num_elements, bool is_random, float value)
@@ -39,77 +77,126 @@ __host__ float* initHostMatrix(int num_elements, bool is_random, float value)
 __host__ float* initDeviceFromHostMatrix(float* h_matrix, int num_elements)
 {
     float* d_matrix;
-    cudaMalloc((void**)&d_matrix, num_elements);
-    cudaMemcpy(d_matrix, h_matrix, num_elements, cudaMemcpyFromHostToDevice);
+    cudaMalloc((void**)&d_matrix, sizeof(float)*num_elements);
+    cudaMemcpy(d_matrix, h_matrix, sizeof(float)*num_elements, cudaMemcpyHostToDevice);
 
     return d_matrix;
 }
 
-__global__ float* addBias(float* x, float* bias, int height, int width)
+__global__ void addBias(float* x, float* bias, int height, int width)
 {
     int thread_id = threadIdx.x + blockDim.x*blockIdx.x;
     int bias_id = thread_id % width;
     x[thread_id] = x[thread_id] + bias[bias_id];
 }
 
-__host__ float* addBiasLauncher(float* x, float* bias, int height, int width)
+__host__ void addBiasLauncher(float* x, float* bias, int height, int width)
 {
     int num_elements = height*width;
-    int blocks = threads / 1024 + 1;
+    int blocks = num_elements / 1024 + 1;
     int threads = min(1024, num_elements);
     addBias<<<blocks, threads>>>(x, bias, height, width);
 }
-__host__ void printFloatArray(float *x, int num_elements)
+__host__ void printDeviceFloatArray(float *x, int h, int w, std::string prefix)
 {
-    for (int i = 0; i < num_elements; i++)
-        std::cout << x[i] << " ";
+    float* h_x = (float*) malloc(sizeof(float)*h*w);
+
+    cudaMemcpy(h_x, x, sizeof(float)*h*w, cudaMemcpyDeviceToHost);
+    std::cout << prefix << " \n";
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+        {
+            std::cout << h_x[i*w + j] << " ";
+        }
+        std::cout << std::endl;
+    }
     std::cout << std::endl;
+
+    free(h_x);
 }
 
 int main(int argc, char **argv)
 {
     OutputHWSpecs();
 
-    bool is_random=false;
+    bool is_random=true;
     float* h_x = initHostMatrix(HX*WX, is_random, 1.0);
     float* d_x = initDeviceFromHostMatrix(h_x, HX*WX);
+
     float* h_w = initHostMatrix(HW*WW, is_random, 1.0);
     float* d_w = initDeviceFromHostMatrix(h_w, HW*WW);
-    float* h_b = initHostMatrix(HW, is_random, 1.0);
+
+    float* h_b = initHostMatrix(HW, is_random, -3.5);
     float* d_b = initDeviceFromHostMatrix(h_b, HW);
-    float* h_y = initHostMatrix(HX*HW, is_random, 1.0);
-    float* d_y = initDeviceFromHostMatrix(h_y, HX*HW);
-    
+
+    float* h_y = initHostMatrix(HY*WY, false, 0.0);
+    float* d_y = initDeviceFromHostMatrix(h_y, HY*WY);
+
+    // prints
+    printDeviceFloatArray(d_x, HX, WX, "x init: ");
+    printDeviceFloatArray(d_w, HW, WW, "w init: ");
+    printDeviceFloatArray(d_y, HY, WY, "y init: ");
+
+    float alpha[1] = {1.0};
+    float beta[1] = {0.0};
+
+
     // create cublas handle
-    cublasHanlde_t cublas_handle;
-    cublasCreate(&cublas_handle);
+    cublasHandle_t cublas_handle;
+    cublasCreate_v2(&cublas_handle);
+    cublasStatus_t stat;
+
     // dot product input x by weights w
-    cublasSgemm(
-        'n',
-        't',
-        HX, WW, HW,
-        d_x, HX,
+    stat = cublasSgemm(
+        cublas_handle,
+        CUBLAS_OP_N,
+        CUBLAS_OP_N,
+        //WX, HW, WW,
+        WW, HX, HW,
+        alpha,
         d_w, WW,
-        d_y, HX,
+        d_x, WX,
+        beta,
+        d_y, WW
     );
 
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        printf ("gemm failed");
+        return EXIT_FAILURE;
+    }
+
+    printDeviceFloatArray(d_y, HY, WY, "output after GEMM: ");
+
     // add bias to the product
-    addBiasLauncher(d_y, d_b, HX, HW);
+    addBiasLauncher(d_y, d_b, HY, WY);
+
+    printDeviceFloatArray(d_y, HY, WY, "output after adding bias: ");
 
     //create cudnn handle
     cudnnHandle_t cudnn_handle;
     cudnnCreate(&cudnn_handle);
 
+    auto y_desc = createTensorDescriptor(HY, WY);
+    runCuDnnActivationForward(cudnn_handle, y_desc, d_y);
+    
+    printDeviceFloatArray(d_y, HY, WY, "output after activation: ");
+
     // destroy cublas handle
     cublasDestroy(cublas_handle);
 
     // destroy cudnn handle
-    cudnnDestroy(handle_);
+    cudnnDestroy(cudnn_handle);
 
-    // free device matrices
+    // free host & device memory
     cudaFree(d_x);
     cudaFree(d_w);
     cudaFree(d_b);
     cudaFree(d_y);
+    free(h_x);
+    free(h_w);
+    free(h_b);
+    free(h_y);
     return 0;
 }
